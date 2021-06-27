ac33753d5adca79c82de196cd80bde2678a84786
Grace de la Mora <grace.g.delamora@gmail.com>
2021-06-27 01:04:44 -0400

    Reset project using redux/typescript CRA

--- src/components/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import { Fragment } from 'react';
-
-const Dashboard = () => (
-  <Fragment>
-    <h1>This is the dashboard</h1>
-  </Fragment>
-);
-
-export default Dashboard;
--- src/fauna/db.js
+++
@@ -1,7 +0,0 @@
-import faunadb, { query as q } from 'faunadb';
-
-require('dotenv').config();
-
-const client = new faunadb.Client({ secret: 'fnAEMDb0cDACBN5PrpQlErjDqEjPEuuszteP3FY2' });
-
-export { client, q };
